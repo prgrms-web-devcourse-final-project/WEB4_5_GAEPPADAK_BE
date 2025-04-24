88c3176b19d1d0ed774ba852d1b603837f2fd92d
김하연 <vthyrachel@naver.com>
2025-04-24 14:12:32 +0900

    Chore/3 Project setting (#4)

---
+++ backend/src/test/java/site/kkokkio/KkokkioApplicationTests.java
@@ -0,0 +1,13 @@
+package site.kkokkio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkokkioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

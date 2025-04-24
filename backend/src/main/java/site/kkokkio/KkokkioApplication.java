26974c169014fe3ff570b66b37823bf44f012fe0
ihtblwFIZ <vthyrachel@naver.com>
2025-04-24 12:50:44 +0900

    chore: project init

---
+++ backend/src/main/java/site/kkokkio/KkokkioApplication.java
@@ -0,0 +1,13 @@
+package site.kkokkio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkokkioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkokkioApplication.class, args);
+	}
+
+}

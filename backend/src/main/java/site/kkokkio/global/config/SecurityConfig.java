6b5b8281e0aa87af79fdd79c6e4dc446aaf5b3b6
GODAOS <jmskkr@gmail.com>
2025-04-25 19:45:42 +0900

    docs: 설명 주석 추가

--- backend/src/main/java/site/kkokkio/global/config/SecurityConfig.java
+++ backend/src/main/java/site/kkokkio/global/config/SecurityConfig.java
@@ -28,18 +28,21 @@ public class SecurityConfig {
 			.cors(
 				httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()))
 
-			// CSRF 보호 비활성화 ()
+			// CSRF 보호 비활성화 (REST API + JWT 조합)
 			.csrf(csrf -> csrf.disable())
 
-			// H2 콘솔 접근을 허용하기 위해 frameOptions 비활성화
-			.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+			// HTTP 응답 헤더 설정
+			.headers(headers -> headers
+				// H2 콘솔 접근을 허용하기 위해 frameOptions 비활성화(dev 전용)
+				.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+			)
 
 			// 세션을 사용하지 않는 Stateless로 설정
 			.sessionManagement(session -> session
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			)
 
-			// 요청 URL별 권한 설정
+			// 엔드포인트별 권한 설정
 			.authorizeHttpRequests(authorize -> authorize
 				// 인증 없이 접근 허용할 엔드포인트
 				// .requestMatchers(
@@ -51,24 +54,26 @@ public class SecurityConfig {
 				// 그 외 모든 요청은 인증 필요
 				// .anyRequest().authenticated()
 
-				// 개발을 위해 모두 오픈
+				// 개발을 위해 모두 오픈(운영 배포시 변경 예정)
 				.anyRequest().permitAll()
 			);
 
 		return http.build();
 	}
 
+	// CORS 설정을 분리해 둔 Bean
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		// 모든 출처 허용 (운영 환경 배포 시 수정 필요)
 		configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000")); // 프론트 운영 주소로 변경 필요
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("*"));
-		configuration.setAllowCredentials(true);
-		configuration.addExposedHeader("Authorization");
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용 HTTP 메소드
+		configuration.setAllowedHeaders(Arrays.asList("*")); // HTTP 헤더(모두 허용)
+		configuration.setAllowCredentials(true); // 쿠키 등 자격 증명
+		configuration.addExposedHeader("Authorization"); // 클라이언트 노출 헤더
+
 		// 모든 엔드포인트에 대해 CORS 설정 적용
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}

d1755315ca115e5fb11c812e0484510f8b1b9917
김하연 <vthyrachel@naver.com>
2025-04-25 17:30:50 +0900

    Feat/11 add base entity and swagger (#13)
    * feat: add BaseTimeEntity
    
    * feat: add swagger dependency

---
+++ backend/src/main/java/site/kkokkio/global/util/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package site.kkokkio.global.util;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+	@CreatedDate
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
+	@LastModifiedDate
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+}

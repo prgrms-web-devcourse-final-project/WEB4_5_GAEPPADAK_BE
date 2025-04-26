11c3cb2137d42e590f5896538391a81733cec2ca
dlfjsld1 <ltk900216@gmail.com>
2025-04-26 12:21:56 +0900

    temp commit for merge rebase

---
+++ infra/variable.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "kkokkio"
+}
+
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+
+variable "db_name" {
+  description = "db name"
+  default     = "kkokkio_dev"
+}

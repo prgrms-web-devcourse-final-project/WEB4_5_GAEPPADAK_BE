00560d91927dbc47fee170c0a77142ae93f38964
kyungrae kim <15260002+GODAOS@users.noreply.github.com>
2025-04-28 11:32:25 +0900

    Feat/19 setup rsdata (#20)
    * feat: RsData & Empty add
    
    * feat: ResponseAspect add
    
    * feat: GlobalExceptionHandler add
    
    * feat: ServiceException add
    
    * docs: 설명 주석 추가

---
+++ backend/src/main/java/site/kkokkio/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,53 @@
+package site.kkokkio.global.exception;
+
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import site.kkokkio.global.dto.RsData;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	// @Valid 검증 실패 시
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<RsData<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+		// 검증 오류 메시지들 한줄로 합치기
+		String message = e.getBindingResult().getFieldErrors()
+			.stream()
+			.map(fe -> fe.getField() + " : " + fe.getCode() + " : " + fe.getDefaultMessage())
+			.sorted()
+			.collect(Collectors.joining("\n"));
+
+		// 400 코드로 RsData 생성 후 BAD_REQUEST(400) 상태로 응답
+		return ResponseEntity
+			.status(HttpStatus.BAD_REQUEST)
+			.body(
+				new RsData<>(
+					"400",
+					message
+				)
+			);
+	}
+
+	// 서비스 로직에서 발생한 커스텀 예외(ServiceException) 처리
+	@ResponseStatus // ResponseEntity.status()에 우선권이 있음
+	@ExceptionHandler(ServiceException.class)
+	public ResponseEntity<RsData<Void>> ServiceExceptionHandle(ServiceException ex) {
+
+		return ResponseEntity
+			.status(ex.getStatusCode()) // ex.getStatusCode() → RsData에서 파싱된 HTTP 상태
+			.body(
+				new RsData<>(
+					ex.getCode(),
+					ex.getMessage()
+				)
+			);
+	}
+}

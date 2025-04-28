8475a7b0fdcca26db59ce0f7d940b36f3c421679
GODAOS <jmskkr@gmail.com>
2025-04-29 01:34:12 +0900

    feat: 로그인 설정 및 JWT 토큰 쿠키 설정

--- backend/src/main/java/site/kkokkio/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/site/kkokkio/domain/member/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package site.kkokkio.domain.member.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ public interface MemberRepository extends JpaRepository<Member, UUID> {
 	boolean existsByEmail(String email);
 
 	boolean existsByNickname(String nickname);
+
+	Optional<Member> findByEmail(String email);
 }
--- backend/src/main/java/site/kkokkio/domain/member/service/MemberService.java
+++ backend/src/main/java/site/kkokkio/domain/member/service/MemberService.java
@@ -1,14 +1,21 @@
 package site.kkokkio.domain.member.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import site.kkokkio.domain.member.dto.MemberLoginResponse;
 import site.kkokkio.domain.member.dto.MemberResponse;
 import site.kkokkio.domain.member.dto.MemberSignUpRequest;
 import site.kkokkio.domain.member.entity.Member;
 import site.kkokkio.domain.member.repository.MemberRepository;
 import site.kkokkio.global.enums.MemberRole;
+import site.kkokkio.global.exception.ServiceException;
+import site.kkokkio.global.util.JwtUtils;
 
 @Service
 @RequiredArgsConstructor
@@ -16,15 +23,18 @@ public class MemberService {
 
 	private final MemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
+	private final JwtUtils jwtUtils;
 
+	// 회원 가입
+	@Transactional
 	public MemberResponse createMember(MemberSignUpRequest request) {
 
 		// 중복 검사
 		if (memberRepository.existsByEmail(request.email())) {
-			throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+			throw new ServiceException("409-1", "이미 사용중인 이메일입니다.");
 		}
 		if (memberRepository.existsByNickname(request.nickname())) {
-			throw new IllegalArgumentException("이미 존재하는 닉네임입니다.");
+			throw new ServiceException("409-2", "이미 사용중인 닉네임입니다.");
 		}
 
 		// 비밀번호 암호화
@@ -40,5 +50,28 @@ public class MemberService {
 			.build();
 		memberRepository.save(member);
 		return new MemberResponse(member);
+	}
+
+	// 로그인
+	public MemberLoginResponse loginMember(String email, String password) {
+
+		// 회원 이메일 확인
+		Member member = memberRepository.findByEmail(email).orElseThrow(() ->
+			new ServiceException("404", "존재하지 않는 이메일입니다."));
+
+		// 비밀번호 확인
+		if (!passwordEncoder.matches(password, member.getPasswordHash())) {
+			throw new ServiceException("401", "비밀번호가 올바르지 않습니다.");
+		}
+
+		// JWT 토큰 발생 시 포함할 사용자 정보 설정
+		Map<String, Object> claims = new HashMap<>();
+		claims.put("id", member.getId());
+		claims.put("email", member.getEmail());
+		claims.put("nickname", member.getNickname());
+		claims.put("role", member.getRole());
+		String token = jwtUtils.createToken(claims); // 토큰 생성
+
+		return MemberLoginResponse.of(member, token);
 	}
 }

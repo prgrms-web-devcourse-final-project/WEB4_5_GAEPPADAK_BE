e656a64885d15ad82ec45715e065d92ab91c1ce9
dlfjsld1 <70743878+dlfjsld1@users.noreply.github.com>
2025-04-30 08:54:29 +0900

    Build/6 CI/CD (#43)
    * temp commit for merge rebase
    
    * .gitignore에 infra 추가
    
    # Conflicts:
    #	.gitignore
    
    * .gitignore 수정
    
    * 테라폼 도플러에 gitignore 적용
    
    * 깃헙 액션과 배포 환경에 도플러 적용
    
    * 도플러를 테라폼 변수에 적용 및 코드 수정
    
    * 테라폼이 docker-compose와 동일한 설정으로 redis와 mysql을 올리도록 수정, 테라폼에 aws 서비스의 태그 추가, 도플러 추가. 깃헙 액션 파일로 배포하도록 설정. 도플러로 비밀을 가져와 깃헙액션에 사용하고, 배포할 때도 사용하도록 수정
    
    * 깃헙 액션 배포 파일에서 도플러를 이용해 백엔드 서버를 배포하도록 추가. .gitignore 수정
    
    * deploy 이름 변경. path 일부 변경

---
+++ infra/variable.tf
@@ -0,0 +1,58 @@
+# 도플러 환경변수를 적용해 실행해야 합니다.
+# doppler run -- terraform init
+# doppler run -- terraform plan
+# doppler run -- terraform apply
+# doppler run -- terraform destroy
+
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "kkokkio"
+}
+
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+
+variable "db_name" {
+  description = "db name"
+  default     = "kkokkio_dev"
+}
+
+variable "PASSWORD_1" {
+  description = "password_1 (Provided via Doppler)"
+}
+
+variable "GITHUB_ACCESS_TOKEN_1" {
+  description = "github_access_token_1, read:packages only (Provided via Doppler)"
+}
+
+variable "GITHUB_ACCESS_TOKEN_1_OWNER" {
+  description = "github_access_token_1_owner (Provided via Doppler)"
+}
+
+
+variable "MYSQL_USER_1" {
+  description = "mysql_user_1 (Provided via Doppler)"
+}
+
+variable "MYSQL_USER_2" {
+  description = "mysql_user_2 (Provided via Doppler)"
+}
+
+variable "DB_ROOT_PASSWORD" {
+  description = "DB_ROOT_PASSWORD (Provided via Doppler)"
+}
+
+variable "DB_NAME" {
+  description = "DB_NAME (Provided via Doppler)"
+}
+
+variable "DB_USERNAME" {
+  description = "DB_USERNAME (Provided via Doppler)"
+}
+
+variable "DB_PASSWORD" {
+  description = "DB_PASSWORD (Provided via Doppler)"
+}
+

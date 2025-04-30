b7927b4a8937bdc37ee7f8eeb8d04a8d9671af6c
김하연 <vthyrachel@naver.com>
2025-04-30 15:38:15 +0900

    Refactor/55 pagination meta (#58)
    refactor: separate PaginationMeta

--- backend/src/main/java/site/kkokkio/domain/comment/controller/dto/CommentListResponse.java
+++ backend/src/main/java/site/kkokkio/domain/comment/controller/dto/CommentListResponse.java
@@ -6,32 +6,22 @@ import org.springframework.data.domain.Page;
 
 import lombok.Builder;
 import site.kkokkio.domain.comment.dto.CommentDto;
+import site.kkokkio.global.dto.PaginationMeta;
 
 @Builder
 public record CommentListResponse(
 	List<CommentDto> list,
 	PaginationMeta meta
 ) {
-	@Builder
-	private record PaginationMeta(
-		int page,
-		int size,
-		long totalElements,
-		int totalPages,
-		boolean hasNext,
-		boolean hasPrevious
-	) {
-	}
-
 	public static CommentListResponse from(Page<CommentDto> comments) {
-		PaginationMeta paginationMeta = PaginationMeta.builder()
-			.page(comments.getNumber())
-			.size(comments.getSize())
-			.totalElements(comments.getTotalElements())
-			.totalPages(comments.getTotalPages())
-			.hasNext(comments.hasNext())
-			.hasPrevious(comments.hasPrevious())
-			.build();
+		PaginationMeta paginationMeta = PaginationMeta.of(
+			comments.getNumber(),
+			comments.getSize(),
+			comments.getTotalElements(),
+			comments.getTotalPages(),
+			comments.hasNext(),
+			comments.hasPrevious()
+		);
 
 		return CommentListResponse.builder()
 			.list(comments.getContent())

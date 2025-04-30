69fba577ccdf67fbb9660c75786a12410d4625a8
skvhffpdyd <115456416+skvhffpdyd@users.noreply.github.com>
2025-04-30 20:28:33 +0900

    refactor: 사용하지 않는 메소드 삭제

--- backend/src/main/java/site/kkokkio/domain/post/repository/PostRepository.java
+++ backend/src/main/java/site/kkokkio/domain/post/repository/PostRepository.java
@@ -11,19 +11,4 @@ import org.springframework.data.repository.query.Param;
 import site.kkokkio.domain.post.entity.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-	@Query(value = """
-            SELECT p
-            FROM Post p
-            JOIN PostKeyword pk ON p.id = pk.post.id
-            JOIN Keyword k ON pk.keyword.id = k.id
-            WHERE k.text = :keywordText
-            """,
-		countQuery = """
-                    SELECT COUNT(p)
-                    FROM Post p
-                    JOIN PostKeyword pk ON p.id = pk.post.id
-                    JOIN Keyword k ON pk.keyword.id = k.id
-                    WHERE k.text = :keywordText
-                    """)
-	Page<Post> findPostsByKeywordText(@Param("keywordText") String keywordText, Pageable pageable);
 }

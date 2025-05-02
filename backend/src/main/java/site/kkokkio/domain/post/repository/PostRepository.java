4f608beaa6e158e9125d93ec0d6987f1a3ed7a04
skvhffpdyd <115456416+skvhffpdyd@users.noreply.github.com>
2025-05-02 16:04:54 +0900

    feat: monitoring을 위한 docker-compose 추가 (Prometheus + Grafana + mysql-exporter)

--- backend/src/main/java/site/kkokkio/domain/post/repository/PostRepository.java
+++ backend/src/main/java/site/kkokkio/domain/post/repository/PostRepository.java
@@ -1,12 +1,6 @@
 package site.kkokkio.domain.post.repository;
 
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import site.kkokkio.domain.post.entity.Post;
 

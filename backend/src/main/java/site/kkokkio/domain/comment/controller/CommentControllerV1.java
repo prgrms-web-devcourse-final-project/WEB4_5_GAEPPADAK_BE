b18a54cc80ee05da3f847c572899e6d2084c6111
haeun9988 <haeun9988@naver.com>
2025-05-12 17:31:52 +0900

    add enum

--- backend/src/main/java/site/kkokkio/domain/comment/controller/CommentControllerV1.java
+++ backend/src/main/java/site/kkokkio/domain/comment/controller/CommentControllerV1.java
@@ -1,42 +1,27 @@
 package site.kkokkio.domain.comment.controller;
 
-import org.springdoc.core.annotations.ParameterObject;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 import site.kkokkio.domain.comment.controller.dto.CommentCreateRequest;
 import site.kkokkio.domain.comment.controller.dto.CommentListResponse;
 import site.kkokkio.domain.comment.dto.CommentDto;
-import site.kkokkio.domain.comment.dto.CommentReportRequestDto;
 import site.kkokkio.domain.comment.service.CommentService;
-import site.kkokkio.domain.member.entity.Member;
-import site.kkokkio.global.auth.CustomUserDetails;
-import site.kkokkio.global.auth.annotations.IsActiveMember;
-import site.kkokkio.global.auth.annotations.IsCommentActiveOwner;
-import site.kkokkio.global.auth.annotations.IsCommentOwner;
 import site.kkokkio.global.dto.Empty;
 import site.kkokkio.global.dto.RsData;
 import site.kkokkio.global.exception.doc.ApiErrorCodeExamples;
 import site.kkokkio.global.exception.doc.ErrorCode;
+import site.kkokkio.global.security.CustomUserDetails;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @Tag(name = "Comment API V1", description = "댓글 관련 API 엔드포인트 V1")
 public class CommentControllerV1 {
@@ -44,7 +29,7 @@ public class CommentControllerV1 {
 
 	@Operation(summary = "댓글 목록 조회")
 	@ApiErrorCodeExamples({ErrorCode.POST_NOT_FOUND_3})
-	@GetMapping("/v1/posts/{postId}/comments")
+	@GetMapping("/posts/{postId}/comments")
 	public RsData<CommentListResponse> getCommentList(
 		@PathVariable("postId") Long postId,
 		@ParameterObject @PageableDefault(sort = "likeCount") Pageable pageable
@@ -63,8 +48,7 @@ public class CommentControllerV1 {
 	@Operation(summary = "댓글 작성")
 	@ApiErrorCodeExamples({ErrorCode.TOKEN_EXPIRED, ErrorCode.UNSUPPORTED_TOKEN,
 		ErrorCode.MALFORMED_TOKEN, ErrorCode.CREDENTIALS_MISMATCH, ErrorCode.POST_NOT_FOUND_3})
-	@PostMapping("/v1/posts/{postId}/comments")
-	@IsActiveMember
+	@PostMapping("/posts/{postId}/comments")
 	public RsData<CommentDto> createComment(
 		@PathVariable("postId") Long postId,
 		@AuthenticationPrincipal CustomUserDetails userDetails,
@@ -81,8 +65,7 @@ public class CommentControllerV1 {
 	@ApiErrorCodeExamples({ErrorCode.TOKEN_EXPIRED, ErrorCode.UNSUPPORTED_TOKEN,
 		ErrorCode.MALFORMED_TOKEN, ErrorCode.CREDENTIALS_MISMATCH,
 		ErrorCode.COMMENT_UPDATE_FORBIDDEN, ErrorCode.COMMENT_NOT_FOUND})
-	@PatchMapping("/v1/comments/{commentId}")
-	@IsCommentActiveOwner
+	@PatchMapping("/comments/{commentId}")
 	public RsData<CommentDto> updateComment(
 		@PathVariable("commentId") Long commentId,
 		@AuthenticationPrincipal CustomUserDetails userDetails,
@@ -100,8 +83,7 @@ public class CommentControllerV1 {
 	@ApiErrorCodeExamples({ErrorCode.TOKEN_EXPIRED, ErrorCode.UNSUPPORTED_TOKEN,
 		ErrorCode.MALFORMED_TOKEN, ErrorCode.CREDENTIALS_MISMATCH,
 		ErrorCode.COMMENT_DELETE_FORBIDDEN, ErrorCode.COMMENT_NOT_FOUND})
-	@DeleteMapping("/v1/comments/{commentId}")
-	@IsCommentOwner
+	@DeleteMapping("/comments/{commentId}")
 	public RsData<Empty> deleteComment(
 		@PathVariable("commentId") Long commentId,
 		@AuthenticationPrincipal CustomUserDetails userDetails
@@ -118,8 +100,7 @@ public class CommentControllerV1 {
 		ErrorCode.MALFORMED_TOKEN, ErrorCode.CREDENTIALS_MISMATCH,
 		ErrorCode.COMMENT_LIKE_BAD_REQUEST, ErrorCode.COMMENT_LIKE_FORBIDDEN,
 		ErrorCode.COMMENT_NOT_FOUND})
-	@PostMapping("/v1/comments/{commentId}/like")
-	@IsActiveMember
+	@PostMapping("/comments/{commentId}/like")
 	public RsData<CommentDto> likeComment(
 		@PathVariable("commentId") Long commentId,
 		@AuthenticationPrincipal CustomUserDetails userDetails
@@ -136,8 +117,7 @@ public class CommentControllerV1 {
 	@ApiErrorCodeExamples({ErrorCode.TOKEN_EXPIRED, ErrorCode.UNSUPPORTED_TOKEN,
 		ErrorCode.MALFORMED_TOKEN, ErrorCode.CREDENTIALS_MISMATCH,
 		ErrorCode.COMMENT_UNLIKE_BAD_REQUEST, ErrorCode.COMMENT_LIKE_FORBIDDEN, ErrorCode.COMMENT_NOT_FOUND})
-	@DeleteMapping("/v1/comments/{commentId}/like")
-	@IsActiveMember
+	@DeleteMapping("/comments/{commentId}/like")
 	public RsData<CommentDto> unlikeComment(
 		@PathVariable("commentId") Long commentId,
 		@AuthenticationPrincipal CustomUserDetails userDetails
@@ -147,31 +127,6 @@ public class CommentControllerV1 {
 			"200",
 			"좋아요가 취소되었습니다.",
 			comment
-		);
-	}
-
-	@Operation(summary = "댓글 신고")
-	@ApiErrorCodeExamples({ErrorCode.TOKEN_EXPIRED, ErrorCode.UNSUPPORTED_TOKEN,
-		ErrorCode.MALFORMED_TOKEN, ErrorCode.CREDENTIALS_MISMATCH,
-		ErrorCode.COMMENT_NOT_FOUND,
-		ErrorCode.REPORT_COMMENT_BAD_REQUEST,
-		ErrorCode.REPORT_COMMENT_FORBIDDEN
-	})
-	@PostMapping("/v2/reports/comments/{commentId}")
-	@ResponseStatus(HttpStatus.OK)
-	@IsActiveMember
-	public RsData<Void> reportComment(
-		@PathVariable("commentId") Long commentId,
-		@AuthenticationPrincipal CustomUserDetails userDetails,
-		@Valid @RequestBody CommentReportRequestDto request
-	) {
-		Member reporter = userDetails.getMember();
-
-		commentService.reportComment(commentId, reporter, request);
-
-		return new RsData<>(
-			"200",
-			"정상적으로 댓글 신고가 접수 되었습니다."
 		);
 	}
 }

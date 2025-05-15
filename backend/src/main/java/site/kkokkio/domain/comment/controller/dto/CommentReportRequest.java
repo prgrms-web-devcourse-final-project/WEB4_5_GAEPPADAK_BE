eafca51d46558457e2ea8d40f28c08715cb17ec4
haeun9988 <haeun9988@naver.com>
2025-05-15 15:12:20 +0900

    add repository

--- backend/src/main/java/site/kkokkio/domain/comment/controller/dto/CommentReportRequest.java
+++ backend/src/main/java/site/kkokkio/domain/comment/controller/dto/CommentReportRequest.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:backend/src/main/java/site/kkokkio/domain/comment/controller/dto/CommentReportRequest.java
 package site.kkokkio.domain.comment.controller.dto;
-========
-package site.kkokkio.domain.report.dto;
->>>>>>>> aff77ed (add post report feature and comment report modify):backend/src/main/java/site/kkokkio/domain/report/dto/CommentReportRequestDto.java
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;

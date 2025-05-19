47c3f345ccb1e323f4ad16a46272b5046d36d351
GODAOS <jmskkr@gmail.com>
2025-05-19 10:46:46 +0900

    fix: 인증 경로 허

--- backend/src/main/java/site/kkokkio/global/config/SecurityConfig.java
+++ backend/src/main/java/site/kkokkio/global/config/SecurityConfig.java
@@ -105,7 +105,7 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		// 모든 출처 허용 (운영 환경 배포 시 수정 필요)
-		configuration.setAllowedOriginPatterns(Arrays.asList(
+		configuration.setAllowedOrigins(List.of(
 			// "https://login.aleph.kr", // 임시 프론트 배포 URL1
 			// "https://www.app4.qwas.shop", // 임시 프론트 배포 URL2
 			"https://web.api.deploy.kkokkio.site:3000", // 프론트 API URL
